52ddb67e9fdc6333b0ccfd8d4eb0acbf51468dc1
Alice Jones <21381891+Pactionly@users.noreply.github.com>
2021-03-26 13:22:39 -0700

    Add images to selection pool; Remove obsolete features

--- features/picture.js
+++ features/picture.js
@@ -4,19 +4,18 @@
  */
 
 module.exports = function(controller) {
-
-    controller.hears('sample','message,direct_message', async(bot, message) => {
-        await bot.reply(message, 'I heard a sample message.');
-    });
-
-    controller.on('message,direct_message', async(bot, message) => {
-        await bot.reply(message, `Echo: ${ message.text }`);
-    });
-
    controller.on('slash_command', async(bot, message) => {
      console.log(message);
+     camel_meme = [
+        'https://www.liveabout.com/thmb/099tc9eKaiZWpueTOIgsTBzSlDU=/1920x1080/filters:no_upscale():max_bytes(150000):strip_icc()/guess-what-day-it-is-hump-day-whoop-whoop-5af49483875db9003696b1cc.jpg',
+       'https://i1.wp.com/bestlifeonline.com/wp-content/uploads/2019/04/its-wednesday-camel-.jpg?fit=1200%2C894&ssl=1',
+       'https://www.digitalmomblog.com/wp-content/uploads/2020/12/yes-we-know-its-hump-day-memes.jpg',
+       'https://i.pinimg.com/originals/dd/84/ae/dd84aeadca46de4cb6550c75a14add1e.jpg',
+       'https://i.pinimg.com/originals/26/c1/e9/26c1e9f9f6ffbe0f3c153a02cbdd5e62.jpg',
+     ]
+
      if (message.command == '/wednesday') {
-         await bot.reply(message,'https://i1.wp.com/bestlifeonline.com/wp-content/uploads/2019/04/its-wednesday-camel-.jpg?fit=1200%2C894&ssl=1');
+         await bot.reply(message, camel_meme[Math.floor(Math.random() * camel_meme.length)]);
      }
  });
 
