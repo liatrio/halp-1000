07f14d2c22cbd783a7a7936e091cfd4f25897533
robertkelly <robert@liatrio.com>
2021-04-11 21:52:19 -0500

    Adding a few short comments for clarity

--- features/daily-image.js
+++ features/daily-image.js
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-
 const axios = require('axios');
 
 module.exports = function(controller) {
@@ -17,16 +16,23 @@ module.exports = function(controller) {
   });
  };
 
+/**  
+ * Using the bing image search api which requires an api key you receive from creating a search resource:
+ *   https://docs.microsoft.com/en-us/bing/search-apis/bing-web-search/create-bing-search-service-resource#create-your-bing-resource
+ */
 async function getImage(bot, message, imageSearchString) {
+  // We could break this out a bit but this works for today...
   let searchUrl = 'https://api.bing.microsoft.com/v7.0/images/search?q='+imageSearchString+'&size=medium&mkt=en-us&count=50'
   let searchResponse = await axios.get(searchUrl, { 
     headers: {
       'Ocp-Apim-Subscription-Key': process.env.BING_SEARCH_API_KEY
     }
   })
+  // Grabbing a random image from the results
   let imageResult = searchResponse.data.value[Math.floor(Math.random() * searchResponse.data.value.length)];
   console.log(`Image web search url: ${imageResult.webSearchUrl}`);
   console.log(`Image content url: ${imageResult.contentUrl}`);
+  // Respond with the contentUrl of the image which is set to "medium" resolution in our search string
   await bot.reply(message, imageResult.contentUrl);
 };
 
@@ -35,6 +41,7 @@ function getImageQueryString(messageText){
     return messageText;
   }
   let day = new Date().getDay();
+  // Just to make sure we're getting some default funny stuff...
   let searchString = 'funny+meme';
   console.log("Today is day #"+day);
   switch(day) {
